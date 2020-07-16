b7e2bb3f0967c224e0054ac27657b555297e6e89
Taeil Kwak <taeil2@gmail.com>
2020-07-16 14:52:09 -0500

    added doubly linked list

--- linkedList.js
+++ linkedList.js
@@ -149,6 +149,168 @@ class LinkedList {
   }
 }
 
+class _doubleNode {
+  constructor(value, prev, next) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  insertFirst(item) {
+    if (this.head === null) {
+      this.head = new _doubleNode(item, null, null);
+      this.tail = this.head;
+    } else {
+      let secondNode = this.head;
+      this.head = new _doubleNode(item, null, secondNode);
+      secondNode.prev = this.head;
+    }
+  }
+  insertLast(item) {
+    if (this.tail === null) {
+      this.tail = new _doubleNode(item, null, null);
+      this.head = this.tail;
+    } else {
+      let secondToLastNode = this.tail;
+      this.tail = new _doubleNode(item, secondToLastNode, null);
+      secondToLastNode.next = this.tail;
+    }
+  }
+  insertBefore(newItem, existingItem) {
+    // Start at the head
+    let currNode = this.head;
+    let prevNode;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    while (currNode.value !== existingItem) {
+      /* Return null if it's the end of the list
+        and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking
+        prevNode = currNode;
+        currNode = currNode.next;
+      }
+    }
+    let newNode = new _Node(newItem, prevNode, currNode);
+    prevNode.next = newNode;
+    currNode.prev = newNode;
+  }
+  insertAfter(newItem, existingItem) {
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    while (currNode.value !== existingItem) {
+      /* Return null if it's the end of the list
+        and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking
+        currNode = currNode.next;
+      }
+    }
+    // The existing item is the last item
+    if (currNode.next === null) {
+      this.insertLast(newItem);
+    } else {
+      let newNode = new _Node(newItem, currNode, currNode.next);
+      currNode.next = newNode;
+      currNode.prev = newNode;
+    }
+  }
+  insertAt(newItem, position) {
+    // Start at the head
+    let currNode = this.head;
+    let prevNode;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    for (let i = 1; i < position; i++) {
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking
+        prevNode = currNode;
+        currNode = currNode.next;
+      }
+    }
+
+    // currNode = item before the new item
+    let newNode = new _Node(newItem, prevNode, currNode);
+    prevNode.next = newNode;
+    currNode.prev = newNode;
+  }
+  remove(item){
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    // Start at the head
+    let currNode = this.head;
+    // Keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      // Save the previous node
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+    currNode.next.prev = previousNode;
+  }
+  find(item) {
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list
+        and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+}
+
 function main() {
   const SLL = new LinkedList;
 
@@ -164,12 +326,26 @@ function main() {
   SLL.insertAfter('Hotdog', 'Helo');
   SLL.insertAt('Kat', 3);
   SLL.remove('Tauhida');
-  SLL.find('Starbuck').next = SLL.find('Boomer')
+
   // Apollo, Athena, Kat, Boomer, Helo, Hotdog, Husker, Starbuck
+
+  const DLL = new DoubleLinkedList;
+
+  DLL.insertFirst('Aquaria');
+  DLL.insertLast('Caprica');
+  DLL.insertLast('Gemenon');
+  DLL.insertLast('Picon');
+  DLL.insertLast('Sagittaron');
+  DLL.insertLast('Tauron');
+  DLL.remove('Picon');
+
+  // Aquaria, Caprica, Gemenon, Sagittaron, Tauron
+
   // console.log(SLL.find('Husker'));
   // console.log(findPrevious('Boomer', SLL));
+  // console.log(cycle(SLL));
 
-  console.log(cycle(SLL));
+  console.log(display(DLL));
 }
 
 main();
@@ -350,8 +526,8 @@ function cycle (lst){
       if (currNode.next === seenArray[i]){
         return true;
       } else {
-        
-        
+
+
       }
       currNode = currNode.next;
     }
